2c060c7f427cf5c7111d4541494d1b48df9612fe
Nurlan270 <nurlanismzade@gmail.com>
2024-11-09 19:37:22 +0400

    favicon added

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -19,7 +19,10 @@
     <meta property="og:url" content="{{ config('app.url') }}"/>
     <meta property="og:site_name" content="Music X"/>
 
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 
     <title>{{ config('app.name') }} - Tune into a world of sound crafted just for you.</title>
 
