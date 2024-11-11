77960d599a70ecc16b33c62dafa891317f3638c4
Nurlan270 <nurlanismzade@gmail.com>
2024-11-11 13:00:03 +0400

    Yandex Metrics added

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -11,6 +11,34 @@
     <meta name="description"
           content="Tune into a world of sound crafted just for you."/>
 
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(98879750, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/98879750" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
+
     {{--  Open Graph Meta (OG)  --}}
     <meta property="og:image"
           content="{{ asset('assets/og-card.png') }}"/>
@@ -30,7 +58,7 @@
 </head>
 <body class="welcome-gradient h-screen overflow-hidden">
 
-    @yield('content')
+@yield('content')
 
 </body>
 </html>
